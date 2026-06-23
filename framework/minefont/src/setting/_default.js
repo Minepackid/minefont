b1da70f0cebee5ac98204b89d7f83fa8ae07417e
Minepackid <minepacknode@gmail.com>
2026-06-23 13:04:22 +0700

    Add default styles for MineFont component

---
+++ framework/minefont/src/setting/_default.js
@@ -0,0 +1,31 @@
+/* ======================================================
+   MineFont - Font SVG For Minepacknode!
+   ====================================================== */
+
+.mc {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 1em;
+  height: 1em;
+  background-color: currentColor;
+  mask-size: contain;
+  mask-repeat: no-repeat;
+  mask-position: center;
+  -webkit-mask-size: contain;
+  -webkit-mask-repeat: no-repeat;
+  -webkit-mask-position: center;
+  vertical-align: -0.225em;
+  flex-shrink: 0;
+  font-style: normal;
+  line-height: 1;
+}
+
+.mc-text {
+  margin: 0 0.15em 0 0.05em;
+}
+
+.mc-only {
+  vertical-align: baseline;
+  margin: 0;
+}
